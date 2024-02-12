efa58d5deb63385663e3bfa6dc758476fdb819df
Jack Le Hamster <jacklehamster@gmail.com>
2024-02-11 23:16:33 -0800

    Auto-commit: 2024-02-11 23:16:33
    Files:
    bun.lockb
    package.json
    src/List.ts
    
    https://github.com/jacklehamster/autocommit

--- src/List.ts
+++ src/List.ts
@@ -1,11 +1,13 @@
+import { Val } from "dok-types";
+
 export interface List<T> {
-  readonly length: number;
+  readonly length: Val<number>;
   at(index: number): T | undefined;
 }
 
 export function forEach<T>(list: List<T> | undefined, callback: (value: T | undefined, index: number) => void): void {
   if (list) {
-    for (let i = 0; i < list.length; i++) {
+    for (let i = 0; i < list.length.valueOf(); i++) {
       const elem = list.at(i);
       callback(elem, i);
     }
@@ -14,7 +16,7 @@ export function forEach<T>(list: List<T> | undefined, callback: (value: T | unde
 
 export function map<T, R>(list: List<T>, callback: (value: T | undefined, index: number) => R): (R | undefined)[] {
   const r: (R | undefined)[] = []
-  for (let i = 0; i < list.length; i++) {
+  for (let i = 0; i < list.length.valueOf(); i++) {
     const elem = list.at(i);
     r.push(callback(elem, i));
   }
